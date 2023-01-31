0828166a06c82444750d7d4273d4d20767792d79
Klym-Chopyk <chopyk.klym@gmail.com>
2023-01-31 21:16:45 +0200

    Init

---
+++ JAVASCRIPT/JS_2/js/test.js
@@ -0,0 +1,17 @@
+console.log(document);
+const userNumber = Number(prompt('Задумайте число!'));
+if (userNumber) {
+}
+else {
+  alert('Задумайте число!')
+}
+const userResponse = Number(alert('Помножте його на 2!'));
+const userResponseNumber = Number(alert('Додайте до отриманого результату 7!'));
+const magicNumber = Number(prompt('Введіть отриманий результат'));
+if (magicNumber) {
+}
+else {
+  alert('Введіть отриманий результат')
+}
+const userResult = (alert(`Ваше число ${userNumber * 2 + 7}`));
+console.log(userResult);
